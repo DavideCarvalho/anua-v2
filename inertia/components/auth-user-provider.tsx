1e28a827d6dab278a669e7d9e174501833117a37
Davi de Carvalho <davi.carvalho96@gmail.com>
2026-02-26 18:15:35 -0300

    fix: simplify auth provider to use Inertia initialUser only (which already has effectiveUser)

--- inertia/components/auth-user-provider.tsx
+++ inertia/components/auth-user-provider.tsx
@@ -1,8 +1,6 @@
 import { router } from '@inertiajs/react'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
 import type { PropsWithChildren } from 'react'
 import { useEffect, useState } from 'react'
-import { authUserQueryKey, useAuthUserQueryOptions } from '../hooks/queries/use_auth_user'
 import type { SharedProps } from '../lib/types'
 import { useAuthStore } from '../stores/auth_store'
 
@@ -12,7 +10,6 @@ interface AuthUserProviderProps extends PropsWithChildren {
 
 export function AuthUserProvider({ children, initialUser }: AuthUserProviderProps) {
   const [sharedUser, setSharedUser] = useState<SharedProps['user']>(initialUser)
-  const queryClient = useQueryClient()
   const setUser = useAuthStore((state) => state.setUser)
   const clearUser = useAuthStore((state) => state.clearUser)
 
@@ -28,26 +25,13 @@ export function AuthUserProvider({ children, initialUser }: AuthUserProviderProp
   }, [])
 
   useEffect(() => {
-    queryClient.setQueryData(authUserQueryKey, sharedUser)
-
     if (sharedUser) {
       setUser(sharedUser)
       return
     }
 
     clearUser()
-  }, [sharedUser, queryClient, setUser, clearUser])
-
-  const { data } = useQuery(useAuthUserQueryOptions(sharedUser))
-
-  useEffect(() => {
-    if (data) {
-      setUser(data)
-      return
-    }
-
-    clearUser()
-  }, [data, setUser, clearUser])
+  }, [sharedUser, setUser, clearUser])
 
   return <>{children}</>
 }
